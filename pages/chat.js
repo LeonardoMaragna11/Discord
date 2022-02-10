8af1b3916420215c7a6a225e9ab0973ba5961603
Leonardo Maragna <86269559+LeonardoMaragna11@users.noreply.github.com>
2022-02-09 21:45:33 -0300

    Update chat.js

--- pages/chat.js
+++ pages/chat.js
@@ -216,7 +216,7 @@ function MessageList(props) {
                             }}
                         >
                             <Text tag="strong">
-                                {mensagem.de}
+                                {props.username}
                             </Text>
                             <Text
                                 styleSheet={{
